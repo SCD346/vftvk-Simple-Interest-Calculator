9ff33d1558a55b1caf7baa2ffaa7074991ad9751
steve_D <scd346@nyu.edu>
2021-08-23 20:21:47 -0500

    styling update, responsive

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+const min = 1
+const max = 100
+const select = document.getElementById('selectElementId');
+
+for (var i = min; i<=max; i++){
+    var opt = document.createElement('option');
+    opt.value = i;
+    opt.innerHTML = i;
+    select.appendChild(opt);
+}
+
 function compute()
 {
-    console.log("click")
-    // p = document.getElementById("principal").value;
-
+    const amount = document.getElementById('principal').value
+    const rate = document.getElementById('rate').value
+    const years = document.getElementById('selectElementId').value
+    // console.log(amount, rate, years)
 }
 
