9fc1abd3835d5657b9da27150b9b699aac5fdee5
steve_D <scd346@nyu.edu>
2021-08-24 09:11:41 -0500

    logis complete, style touched up

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+//No. of Years dropdown setup, (avoids lengthly HTML)
 const min = 1
 const max = 100
 const select = document.getElementById('selectElementId');
+
+
 
 for (var i = min; i<=max; i++){
     var opt = document.createElement('option');
@@ -11,9 +14,39 @@ for (var i = min; i<=max; i++){
 
 function compute()
 {
+    const startingYear = new Date().getFullYear()
+    // console.log(typeof(startingYear))
     const amount = document.getElementById('principal').value
     const rate = document.getElementById('rate').value
-    const years = document.getElementById('selectElementId').value
+    const rateT = rate/100
+    const years = parseInt(document.getElementById('selectElementId').value)
+    const interestAmount = (amount * rateT * years)
+
+    // const interestDisplay = document.querySelector('.range-slider-value')
+    // interestDisplay.innerHTML = `${rate}%`;
+
+
+
+    // console.log(interestAmount)
     // console.log(amount, rate, years)
+    const message = `
+        <span>If you deposit <span id="highlighter">${amount}</span>,</span><br>
+        <span>at an interest rate of <span id="highlighter">${rate}%</span>.</span><br>
+        <span>You will receive an amount of <span id="highlighter">${interestAmount}</span>,</span><br>
+        <span>in the year <span id="highlighter">${startingYear + years}</span></span>
+        `
+    // console.log(message)
+    document.getElementById("result-message").innerHTML = message;
+}
+
+
+//Slider live update
+const sliderRate = document.getElementById('rate')
+sliderRate.addEventListener('input', sliderRateChange)
+
+function sliderRateChange() {
+    console.log(this.value)
+    const rateLocation = document.querySelector('.range-slider-value')
+    rateLocation.innerText = parseFloat(this.value).toFixed(2) + '%'
 }
 
